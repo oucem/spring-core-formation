7bb984ed891d571a96c2c4ba6c36dc9fde2be683
oucem <rabhi.houcem@gmail.com>
2020-06-12 02:15:42 +0200

    make the app supports web context

--- src/test/java/com/certification/springcoreformation/SpringCoreFormationApplicationTests.java
+++ src/test/java/com/certification/springcoreformation/SpringCoreFormationApplicationTests.java
@@ -3,14 +3,13 @@ package com.certification.springcoreformation;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
 
 @SpringBootTest
 class SpringCoreFormationApplicationTests {
 
     @Autowired
-    AnnotationConfigApplicationContext appCtx;
+    GenericWebApplicationContext appCtx;
 
     @Test
     void contextLoads() {
